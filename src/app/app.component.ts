0d174905a06f04251267df4d5eec9cd4724cfbc8
Eric <ericetienneortiz@gmail.com>
2023-10-26 19:29:58 -0300

    undo title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hosting manager!';
+  title = 'Bienvenidos a hosting manager!';
   year = new Date().getFullYear();
 }
