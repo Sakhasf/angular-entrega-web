526038afdd4933d1b507af0b4b16f89355dbd10f
Eric <ericetienneortiz@gmail.com>
2023-11-20 17:52:06 -0300

    modify enviroment to deploy

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
     production: true,
-    apiUrl: 'http://localhost:3000/' // para docker
-//    apiUrl: 'https://servers-and-webs-api.onrender.com/' //deploy 
+//    apiUrl: 'http://localhost:3000/' // para docker
+    apiUrl: 'https://servers-and-webs-api.onrender.com/' //deploy 
 };
